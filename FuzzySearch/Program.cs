457e24112289699477b0a661658a3b81a0bdb92a
MohammadMahdi Jarrahi <Jarrahi.MM@gmail.com>
2019-07-14 11:43:47 +0430

    add clases

---
+++ FuzzySearch/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            String line=Console.ReadLine();
+            Console.WriteLine(line);
+            Console.ReadLine();
+        }
+    }
+}
